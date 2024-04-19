6c411585be108d987d74c0278bbbab72f1ea5c3c
Light Course <sn67@inbox.ru>
2024-04-19 16:36:44 +0300

    init

--- game.js
+++ game.js
@@ -27,13 +27,12 @@ let level = [
 //let levelGraph = new Array(level.length).fill(0).map(el => new Array(level[0].length).fill(0));
 
 let blocks = [];
-let sizeOneBlock = 32;
+let sizeOneBlock = 64;
 
 for (var i = 0; i < level.length; i++) {
   for (var j = 0; j < level[i].length; j++) {
 
     if (level[i][j].b == 9) {
-
       blocks.push(game.newRectObject({
         x: sizeOneBlock * j,
         y: sizeOneBlock * i,
@@ -41,7 +40,15 @@ for (var i = 0; i < level.length; i++) {
         h: sizeOneBlock,
         fillColor: "grey",
       }));
-
+    }
+    else if (level[i][j].p != 0) {
+      blocks.push(game.newRectObject({
+        x: sizeOneBlock * j,
+        y: sizeOneBlock * i,
+        w: sizeOneBlock,
+        h: sizeOneBlock,
+        fillColor: "black",
+      }));
     }
   }
 }
@@ -76,7 +83,7 @@ for (var i = 0; i < level.length; i++) {
 //let masLevelGraph = new Graph(levelGraph);
 
 
-let playerSpeed = 2;
+
 
 let playerBody = game.newRectObject({
   x: 0,
@@ -87,44 +94,85 @@ let playerBody = game.newRectObject({
 })
 
 let playerTop = game.newRectObject({
-  x: 0,
+  x: sizeOneBlock / 12,
   y: -2,
-  w: sizeOneBlock,
+  w: sizeOneBlock / 1.2,
   h: 2,
-  fillColor: "yellow",
+  fillColor: "red",
 })
 let playerBottom = game.newRectObject({
-  x: 0,
+  x: sizeOneBlock / 12,
   y: sizeOneBlock,
-  w: sizeOneBlock,
+  w: sizeOneBlock / 1.2,
   h: 2,
-  fillColor: "yellow",
+  fillColor: "red",
 })
 let playerLeft = game.newRectObject({
   x: -2,
-  y: 0,
+  y: sizeOneBlock / 12,
   w: 2,
-  h: sizeOneBlock,
-  fillColor: "yellow",
+  h: sizeOneBlock / 1.2,
+  fillColor: "red",
 })
 let playerRight = game.newRectObject({
   x: sizeOneBlock,
-  y: 0,
+  y: sizeOneBlock / 12,
   w: 2,
-  h: sizeOneBlock,
-  fillColor: "yellow",
+  h: sizeOneBlock / 1.2,
+  fillColor: "red",
+})
+
+let playerCenter = game.newRectObject({
+  x: sizeOneBlock / 2,
+  y: sizeOneBlock / 2,
+  w: sizeOneBlock / 20,
+  h: sizeOneBlock / 20,
+  fillColor: "red",
 })
 
 let player = game.newMesh({
   x: sizeOneBlock,
   y: sizeOneBlock,
   angle: 0,
-  add: [playerBody, playerTop, playerBottom, playerLeft, playerRight]
+  add: [playerBody, playerTop, playerBottom, playerLeft, playerRight, playerCenter]
 });
 
-player.speed = 0.8;
+player.speed = sizeOneBlock / 40;
 player.nowX = 1;
 player.nowY = 1;
+player.plantBomb = false;
+player.canBombsNum = 2;
+player.currentBombNum = 0;
+player.bombsMas = [
+  {
+    bomb: game.newRectObject({
+      x: sizeOneBlock,
+      y: sizeOneBlock,
+      w: sizeOneBlock,
+      h: sizeOneBlock,
+      fillColor: "red",
+    }),
+    planting: false,
+    timer: pjs.OOP.newTimer(2000, function () {
+      player.bombsMas[0].planting = false;
+      blocks.splice(blocks.indexOf(player.bombsMas[0].bomb), 1);
+    })
+  },
+  {
+    bomb: game.newRectObject({
+      x: 0,
+      y: 0,
+      w: sizeOneBlock,
+      h: sizeOneBlock,
+      fillColor: "red",
+    }),
+    planting: false,
+    timer: pjs.OOP.newTimer(2000, function () {
+      player.bombsMas[1].planting = false;
+      blocks.splice(blocks.indexOf(player.bombsMas[1].bomb), 1);
+    })
+  }
+];
 
 /*//////////////////////////////////////////////////////////////////////////*/
 
@@ -139,17 +187,17 @@ game.newLoop('myGame', function () {
   //pjs.camera.follow(player, 10);
 
   player.draw();
-  player.nowX = Math.round(player.x / sizeOneBlock * 2);
-  player.nowY = Math.round(player.y / sizeOneBlock * 2);
+  player.nowX = Math.round(player.x / sizeOneBlock);
+  player.nowY = Math.round(player.y / sizeOneBlock);
 
-  //console.log(playerBody.getPosition().x);
-  //console.log(level[player.nowX][player.nowY - 1].b);
+  console.log(player.currentBombNum);
+
+
 
   for (var i = 0; i < level.length; i++) {
     for (var j = 0; j < level[i].length; j++) {
 
       if (level[i][j].b == 9) {
-
         game.newRectObject({
           x: sizeOneBlock * j,
           y: sizeOneBlock * i,
@@ -157,8 +205,17 @@ game.newLoop('myGame', function () {
           h: sizeOneBlock,
           fillColor: "grey",
         }).draw();
-
       }
+      else if (level[i][j].p != 0) {
+        game.newRectObject({
+          x: sizeOneBlock * j,
+          y: sizeOneBlock * i,
+          w: sizeOneBlock,
+          h: sizeOneBlock,
+          fillColor: "black",
+        }).draw();
+      }
+
     }
   }
 
@@ -166,29 +223,54 @@ game.newLoop('myGame', function () {
 
 
 
-  if (key.isDown("D")) {
+  if (key.isDown("D") || key.isDown("RIGHT")) {
     player.moving = true;
     player.arrow = 'right';
-    if (!playerRight.isArrIntersect(blocks)) {
+    if (!playerRight.isArrIntersect(blocks) || playerCenter.isArrIntersect(blocks)) {
       player.setPosition(pjs.vector.point(playerBody.getPosition().x + player.speed, playerBody.getPosition().y));
     }
 
-  } else if (key.isDown("A")) {
+  } else if (key.isDown("A") || key.isDown("LEFT")) {
     player.moving = true;
     player.arrow = 'left';
-    player.setPosition(pjs.vector.point(playerBody.getPosition().x - player.speed, playerBody.getPosition().y));
+    if (!playerLeft.isArrIntersect(blocks) || playerCenter.isArrIntersect(blocks)) {
+      player.setPosition(pjs.vector.point(playerBody.getPosition().x - player.speed, playerBody.getPosition().y));
+    }
   }
 
-  if (key.isDown("W")) {
+  if (key.isDown("W") || key.isDown("UP")) {
     player.moving = true;
     player.arrow = 'up';
-    player.setPosition(pjs.vector.point(playerBody.getPosition().x, playerBody.getPosition().y - player.speed));
+    if (!playerTop.isArrIntersect(blocks) || playerCenter.isArrIntersect(blocks)) {
+      player.setPosition(pjs.vector.point(playerBody.getPosition().x, playerBody.getPosition().y - player.speed));
+    }
 
-  } else if (key.isDown("S")) {
+  } else if (key.isDown("S") || key.isDown("DOWN")) {
     player.moving = true;
     player.arrow = 'down';
-    player.setPosition(pjs.vector.point(playerBody.getPosition().x, playerBody.getPosition().y + player.speed));
+    if (!playerBottom.isArrIntersect(blocks) || playerCenter.isArrIntersect(blocks)) {
+      player.setPosition(pjs.vector.point(playerBody.getPosition().x, playerBody.getPosition().y + player.speed));
+    }
   }
+
+
+  if (key.isPress("Z")) {
+    if (!player.bombsMas[player.currentBombNum].planting) {
+      player.bombsMas[player.currentBombNum].planting = true;
+      player.bombsMas[player.currentBombNum].bomb.setPosition(pjs.vector.point(player.nowX * sizeOneBlock, player.nowY * sizeOneBlock));
+      blocks.push(player.bombsMas[player.currentBombNum].bomb);
+      player.bombsMas[player.currentBombNum].timer.restart();
+      if (player.canBombsNum > player.currentBombNum + 1) player.currentBombNum += 1;
+      else if (player.canBombsNum > 0) player.currentBombNum -= 1;
+    }
+
+  }
+
+  player.bombsMas.forEach(element => {
+    if (element.planting) {
+      element.bomb.draw();
+    }
+  });
 
 
 
